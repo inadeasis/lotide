6f182645fb648c1f0c6e547d3bb2f0effb41e00b
Ina Ricci <ina_ricci@hotmail.com>
2023-11-18 15:40:03 -0800

    modified code

--- tail.js
+++ tail.js
@@ -14,3 +14,5 @@ const tail = (arr) => {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words[0], "Lighthouse");
+assertEqual(words[1], "Labs")
