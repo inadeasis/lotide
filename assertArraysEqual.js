a37ffa879dbf60e3997adca52b3d81b0ad0d8cd1
Ina Ricci <ina_ricci@hotmail.com>
2023-11-22 23:52:15 -0800

    first push

---
+++ assertArraysEqual.js
@@ -0,0 +1,38 @@
+const assertEqual = (actual, expected) => {
+  if (actual == expected){
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const eqArrays = (actual, expected) =>{
+    if (actual.length != expected.length) {
+        return false;
+    }
+        for (let i = 0; i < actual.length; i++) {
+            if (actual[i] !== expected[i]) {
+                return false;
+            } 
+        }
+        return true;
+    }
+
+// Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+const assertArraysEqual = (actual, expected) =>{
+  const inspect = require('util').inspect;
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  }else{
+    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+
+// TEST CODE
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
+assertArraysEqual(eqArrays([5, 6, 3], [4, 2, 1]),false);
+assertArraysEqual(eqArrays([6, 3], [4, 2, 1]),false);
+assertArraysEqual(eqArrays([5,4,6],[5,4,6]),true);
