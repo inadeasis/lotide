ea90774f0d53bcce3cf92290573449c6201ea0f1
Ina Ricci <ina_ricci@hotmail.com>
2023-11-22 23:35:55 -0800

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -9,19 +9,14 @@ const assertEqual = (actual, expected) => {
 const eqArrays = (actual, expected) =>{
     if (actual.length != expected.length) {
         return false;
-    } else {
-        let result = false;
+    }
         for (let i = 0; i < actual.length; i++) {
             if (actual[i] !== expected[i]) {
                 return false;
-            } else {
-                result = true;
-            }
+            } 
         }
-        return result;
+        return true;
     }
-}
-
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
