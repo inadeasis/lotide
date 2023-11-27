7e300f0f2f6e943eef8dbfdba08705dd51bd827e
Ina Ricci <ina_ricci@hotmail.com>
2023-11-27 05:37:13 -0800

    fixed bugs

--- middle.js
+++ middle.js
@@ -32,7 +32,7 @@ const middle = (array) => {
   let middle = []
 
   if ((array.length === 1) || (array.length === 2)){
-    
+    return middle
   } else if (array.length %2 === 0) {
     middle.push(array[Math.floor(array.length / 2)-1], array[Math.floor(array.length / 2)])
 
