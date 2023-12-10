ba3fa2860a764d56fbaa3717db03c99158e06cbd
Ina Ricci <ina_ricci@hotmail.com>
2023-12-10 10:44:51 -0800

    refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,20 @@
-const assertArraysEqual = require("../assertArraysEqual")
-const middle = require("../middle")
+// const assertArraysEqual = require("../assertArraysEqual")
+// const middle = require("../middle")
 
-//TEST
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+// //TEST
+// assertArraysEqual(middle([1, 2]),[])
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
+// assertArraysEqual(middle([1, 2, 3, 4, 5,6]),[3,4])
 
-assertArraysEqual(middle([1, 2]),[])
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
-assertArraysEqual(middle([1, 2, 3, 4, 5,6]),[3,4])
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+
+describe("#middle", () => {
+  it("returns [] for [1,2]", () => {
+    assert.strictEqual(middle([1,2]), []);
+  });
+     it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.strictEqual(middle([1, 2, 3, 4, 5]), [3]); 
+   });
+});
