419a22e551a151558bf90d3be13b8fd206ddc4e4
Ina Ricci <ina_ricci@hotmail.com>
2023-12-10 10:02:07 -0800

    refactored

--- middle.js
+++ middle.js
@@ -1,13 +1,3 @@
-const eqArrays = require("./eqArrays")
-const assertArraysEqual = require("./assertEqual")
-
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([5, 6, 3], [4, 2, 1]);
-assertArraysEqual([6, 3], [4, 2, 1]);
-assertArraysEqual([5,4,6],[5,4,6]);
-
 // Implement middle which will take in an array and return the middle-most element(s) of the given array.
 const middle = (array) => {
   let middle = []
