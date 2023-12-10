78d3d82f0701b4ec5ec18bd9d4ecf93dc7081d7e
Ina Ricci <ina_ricci@hotmail.com>
2023-12-10 10:46:16 -0800

    refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -12,9 +12,9 @@ const middle   = require('../middle');
 
 describe("#middle", () => {
   it("returns [] for [1,2]", () => {
-    assert.strictEqual(middle([1,2]), []);
+    assert.deepEqual(middle([1,2]), []);
   });
      it("returns [3] for [1, 2, 3, 4, 5]", () => {
-    assert.strictEqual(middle([1, 2, 3, 4, 5]), [3]); 
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]); 
    });
 });
