8eef6c9acbacd94f961cc511e37e1b3a851b0267
Ina Ricci <ina_ricci@hotmail.com>
2023-12-22 17:33:13 -0800

    fixed code and errors

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,31 +1,18 @@
 const assertEqual = require("./assertEqual")
-const eqArrays = require("./eqArrays")
-
-// Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
-
-const eqObjects = (obj1, obj2) =>{
-  let arr1  = [];
-  let arr2 = [];
-  for (i in obj1){
-    arr1.push(Object.keys(obj1[i]))
-  }
-  for (j in obj2){
-    arr2.push(Object.keys(obj2[j]))
-  }
-  if (arr1.length === arr2.length){
-    return true
-  }
-return false
-}
+const eqObjects = require("./eqObjects")
 
 // Implement assertObjectsEqual which will take in two objects and console.log an appropriate message to the console.
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  console.log(`Example label: ${inspect(actual)}`);
+  
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
 
 };
-
 
 const pets = { 
   dog: "Arya",
@@ -40,5 +27,5 @@ const petObj = {
 }
 
 eqObjects(pets , petObj); // => true
-assertEqual(eqObjects(pets , petObj), true);
+assertEqual(eqObjects(pets , petObj), false);
 assertObjectsEqual(assertEqual(eqObjects(pets , petObj), false), false)
--- test/assertArraysEqualTest.js
+++
@@ -1,9 +0,0 @@
-const assertArraysEqual = require("../assertArraysEqual")
-
-// TEST CODE
-assertArraysEqual(null,[])
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([5, 6, 3], [4, 2, 1]);
-assertArraysEqual([6, 3], [4, 2, 1]);
-assertArraysEqual([5,4,6],[5,4,6]);
