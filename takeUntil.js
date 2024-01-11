a49d274a8dedd90ce1ba23a2cea68e20ac3405af
Ina Ricci <ina_ricci@hotmail.com>
2024-01-11 14:13:59 -0800

    modified main in package JSON to index.js

--- takeUntil.js
+++ takeUntil.js
@@ -29,8 +29,3 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ] )
-
-// expected Output:
-// [ 1, 2, 5, 7, 2 ]
-// ---
-// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
